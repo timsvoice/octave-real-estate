35025e7a127604b26491601c72259d7e222061cf
Timothy Voice <timsethvoice@gmail.com>
2016-10-24 13:47:02 -0400

    updated architecture to use local data

--- index.js
+++ index.js
@@ -1,17 +1,27 @@
 const Handlebars = require('handlebars');
 const Metalsmith = require('metalsmith');
-const markdown = require('metalsmith-markdown');
 const layouts = require('metalsmith-layouts');
 const partials = require('metalsmith-partial');
 const sass = require('metalsmith-sass');
-
-Handlebars.registerPartial('offering', './partials/offering.html');
+const watch = require('metalsmith-watch');
+const serve = require('metalsmith-serve');
 
 Metalsmith(__dirname)
-  .use(markdown())
   .use(layouts({
     'engine': 'handlebars',
+    'partials': 'partials',
   }))
+  .use(serve())
+  .use(
+    watch({
+      paths: {
+        "${source}/**/*": true,
+        "./layouts/**/*": true,
+        "./partials/**/*": true,
+      },
+      livereload: true,
+    })
+  )
   .use(sass({
     outputDir: './css',
     outputStyle: "expanded",
