8504b3a245ff3cd1382d153fe69bc0e632816508
Timothy Voice <timsethvoice@gmail.com>
2016-11-02 11:15:37 -0400

    added sort of working spinner

--- dist/js/media.js
+++ dist/js/media.js
@@ -1,10 +1,11 @@
-$(document).ready(function(){
-  $('#bgvid').on('ended',function(){
-    $('.video-cta').removeClass('hidden');
-    $('.video-cta').addClass('animated fadeIn');
-  });
+$('#bgvid').on('ended',function(){
+  $('.video-cta').removeClass('hide');
+  $('.video-cta').addClass('animated fadeIn');
 });
 
-$(window).on("load", function(){
-  $('.spinner-container').addClass('animated fadeOut');
+$(window).load(function(){
+  $('#spinner-container').addClass('animated fadeOut').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
+    $(this).removeClass('animated fadeOut');
+    $(this).addClass('hide');
+  });
 })
